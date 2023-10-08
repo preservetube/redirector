0bb989ee6637ce49b2d7b4ba87909d8b8f2f4c8f
localhost <o0l2qp3e@duck.com>
2023-10-08 09:48:05 +0200

    adding stuff for www

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express()
 
 app.get('*', (req, res) => {
     const subdomain = req.subdomains[0]
+
     if (!subdomain) return res.redirect(process.env.REDIRECT)
     if (req.path == '/') return res.redirect(process.env.REDIRECT)
+    if (!keys[subdomain]) return res.redirect(`${process.env.REDIRECT}${req.path}`)
 
     if (subdomain == 'media') return res.redirect(`https://link.storjshare.io/raw/${keys[subdomain]}/media${req.path}`)
     else return res.redirect(`https://link.storjshare.io/raw/${keys[subdomain]}/video${req.path}`)
